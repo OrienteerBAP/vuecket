e220327589d0914e46ea909e7537fef2e4fff8a2
Ilia Naryzhny <phantom@ydn.ru>
2020-08-12 21:13:51 -0700

    Step 3 for tutorial: list of messages and submission of new one

--- vuecket-demo/src/main/java/org/orienteer/vuecket/demo/ChatApp.vue
+++ vuecket-demo/src/main/java/org/orienteer/vuecket/demo/ChatApp.vue
@@ -2,8 +2,18 @@
 module.exports = {
     data: function() {
         return {
-            text : ""
+            text : "",
+            messages: []
         }
+    },
+    methods: {
+    	addMessage : function() {
+    		this.messages.push({
+    			message: this.text,
+    			date: new Date()
+    		});
+    		this.text = "";
+    	}
     }
 }
 </script>
