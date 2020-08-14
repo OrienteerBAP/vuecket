eda9f725e3d164fb4b319f3c0558f47d9db67f8f
Ilia Naryzhny <phantom@ydn.ru>
2020-08-13 21:53:17 -0700

    Step 4 for tutorial: data-fiber for colloboration

--- vuecket-demo/src/main/java/org/orienteer/vuecket/demo/ChatApp.java
+++ vuecket-demo/src/main/java/org/orienteer/vuecket/demo/ChatApp.java
@@ -1,15 +1,24 @@
 package org.orienteer.vuecket.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.wicket.Component;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.orienteer.vuecket.VueComponent;
 import org.orienteer.vuecket.descriptor.VueFile;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 @VueFile("ChatApp.vue")
 public class ChatApp extends VueComponent<Void> {
+	
+	private static final IModel<List<JsonNode>> MESSAGES = Model.ofList(new ArrayList<JsonNode>());
 
 	public ChatApp(String id) {
 		super(id);
+		addDataFiber("messages", MESSAGES, true, true, true);
 	}
 
 }
