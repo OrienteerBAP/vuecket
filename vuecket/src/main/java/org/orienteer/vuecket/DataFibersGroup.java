53fdac3f4632afd2c659a985f429e762448f45ff
Ilia Naryzhny <phantom@ydn.ru>
2021-03-29 15:21:45 -0700

    Refactor DataFibersGroup to use predicates

--- vuecket/src/main/java/org/orienteer/vuecket/DataFibersGroup.java
+++ vuecket/src/main/java/org/orienteer/vuecket/DataFibersGroup.java
@@ -5,9 +5,15 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.apache.wicket.model.IDetachable;
 import org.apache.wicket.model.IModel;
+import org.danekja.java.util.function.serializable.SerializablePredicate;
+import org.orienteer.vuecket.DataFiber.DataFiberType;
 
 /**
  * Collection of DataFibers organized in performance oriented way
@@ -15,7 +21,12 @@ import org.apache.wicket.model.IModel;
 public class DataFibersGroup implements Iterable<DataFiber<?>>, IDetachable {
 	
 	private static final long serialVersionUID = 1L;
-
+	
+	public static final SerializablePredicate<DataFiber<?>> PROPERTY_DATAFIBERS = df -> DataFiberType.PROPERTY.equals(df.getType());
+	public static final SerializablePredicate<DataFiber<?>> INIT_DATAFIBERS = DataFiber::shouldInit;
+	public static final SerializablePredicate<DataFiber<?>> UPDATE_DATAFIBERS = DataFiber::shouldUpdate;
+	public static final SerializablePredicate<DataFiber<?>> OBSERVE_DATAFIBERS = DataFiber::shouldObserve;
+	
 	private final List<DataFiber<?>> dataFibers = new ArrayList<DataFiber<?>>();
 	
 	private final Map<String, DataFiber<?>> propertyDataFibers = new HashMap<String, DataFiber<?>>();
@@ -32,21 +43,38 @@ public class DataFibersGroup implements Iterable<DataFiber<?>>, IDetachable {
 		return this;
 	}
 	
+	public Stream<DataFiber<?>> getDataFibersAsStream(Predicate<DataFiber<?>> predicate) {
+		Stream<DataFiber<?>> stream = dataFibers.stream();
+		return predicate!=null?stream.filter(predicate):stream;
+	}
+	
+	public List<DataFiber<?>> getDataFibers(Predicate<DataFiber<?>> predicate) {
+		return getDataFibersAsStream(predicate).collect(Collectors.toList());
+	}
+	
+	public List<String> getDataFibersKeys(Predicate<DataFiber<?>> predicate) {
+		return getDataFibersAsStream(predicate).map(DataFiber::getName).collect(Collectors.toList());
+	}
+	
+	public Map<String, DataFiber<?>> getDataFibersAsMap(Predicate<DataFiber<?>> predicate) {
+		return getDataFibersAsStream(predicate).collect(Collectors.toMap(DataFiber::getName, Function.identity()));
+	}
+	
 	
 	public Map<String, DataFiber<?>> getPropertyDataFibers() {
-		return propertyDataFibers;
+		return getDataFibersAsMap(PROPERTY_DATAFIBERS);
 	}
 
 	public Map<String, DataFiber<?>> getInitDataFibers() {
-		return initDataFibers;
+		return getDataFibersAsMap(INIT_DATAFIBERS);
 	}
 
 	public Map<String, DataFiber<?>> getUpdateDataFibers() {
-		return updateDataFibers;
+		return getDataFibersAsMap(UPDATE_DATAFIBERS);
 	}
 
 	public Map<String, DataFiber<?>> getObserveDataFibers() {
-		return observeDataFibers;
+		return getDataFibersAsMap(OBSERVE_DATAFIBERS);
 	}
 
 	public int size() {
